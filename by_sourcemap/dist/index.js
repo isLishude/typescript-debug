3171049d1bf065385d15767ab19ed1e1b4a62542
islishude <lishude@aliyun.com>
2018-10-07 23:06:17 +0800

    feat: add source_map mode

---
+++ by_sourcemap/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const console_1 = require("console");
+function Add(x, y) {
+    return x + y;
+}
+const z = Add(100, 200);
+console_1.log(z);
+//# sourceMappingURL=index.js.map
---
+++ by_sourcemap/src/index.ts
@@ -0,0 +1,10 @@
+
+import { log } from "console";
+
+function Add(x: number, y: number): number {
+  return x + y;
+}
+
+const z = Add(100, 200);
+
+log(z);
