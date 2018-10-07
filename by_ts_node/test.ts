2472363bd0fcd9121cc232e300410c9be23bf443
islishude <lishude@aliyun.com>
2018-10-07 22:54:09 +0800

    chore: update

--- by_ts_node/test.ts
+++
@@ -1,9 +0,0 @@
-import { log } from "console";
-import { argv, execArgv } from "process";
-
-// node and current file
-log("args", argv);
-// -r ts-node/register
-log("execArgv", execArgv);
-
-log("over");
